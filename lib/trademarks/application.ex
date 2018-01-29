ea2415928e3916324ccf12c351a427bf64d61ca8
mkreyman <mark@kreyman.com>
2018-01-29 10:02:49 -0700

    Added a supervisor with a default configuration

---
+++ lib/trademarks/application.ex
@@ -0,0 +1,20 @@
+defmodule Trademarks.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: Trademarks.Worker.start_link(arg)
+      # {Trademarks.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Trademarks.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule Trademarks.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :httpoison]
+      extra_applications: [:logger, :httpoison],
+      mod: {Trademarks.Application, []}
     ]
   end
 
